756146c28387ca28330bd8d4f2bbd796db19dcc5
Nirav <niravnikhil@gmail.com>
2020-04-04 22:35:08 +0545

    hello!

---
+++ precache-manifest.74d8238ca681ae902bebe4788a98c208.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f50138fed52044bbb641d368ebcfcd",
+    "url": "/clip1.png"
+  },
+  {
+    "revision": "ce49f90dca9755eea43b",
+    "url": "/css/app.c2085847.css"
+  },
+  {
+    "revision": "0b51ed0f66c9b0a7bfe8",
+    "url": "/css/post.1b4a2708.css"
+  },
+  {
+    "revision": "dff0bb00448b08eaeabb",
+    "url": "/css/posts.c6328364.css"
+  },
+  {
+    "revision": "78ff1e13a2e219aa5bdd",
+    "url": "/css/write.322b4fe2.css"
+  },
+  {
+    "revision": "34a1f42a86fe21b627cdfcbbb7bde6e0",
+    "url": "/img/logo.34a1f42a.png"
+  },
+  {
+    "revision": "e6956db22f6b55500046d41bb128484a",
+    "url": "/img/mail.e6956db2.svg"
+  },
+  {
+    "revision": "04a9455d7b70e40d43c0cd371b53f68b",
+    "url": "/img/navBack.04a9455d.svg"
+  },
+  {
+    "revision": "db80653b5ab4a6a28019282dd9120617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce49f90dca9755eea43b",
+    "url": "/js/app.a2ab5582.js"
+  },
+  {
+    "revision": "5169a7aafaafc95bbfc7",
+    "url": "/js/chunk-vendors.d5224835.js"
+  },
+  {
+    "revision": "0b51ed0f66c9b0a7bfe8",
+    "url": "/js/post.4a8f7827.js"
+  },
+  {
+    "revision": "dff0bb00448b08eaeabb",
+    "url": "/js/posts.5cbd84d5.js"
+  },
+  {
+    "revision": "78ff1e13a2e219aa5bdd",
+    "url": "/js/write.c4a116dd.js"
+  },
+  {
+    "revision": "981afc303c5bd333fb17b2c46944a2b1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8232508cadb679ca30b8b87786051c16",
+    "url": "/welcome.png"
+  }
+]);
