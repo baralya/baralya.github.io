bbfc968aeb9c86ea5d078af92737d8d9d114356d
Nirav <niravnikhil@gmail.com>
2020-04-06 23:06:37 +0545

    works in samsunginternet

---
+++ dist/precache-manifest.9a6a64f359f9e6162c246434472d9e9a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f50138fed52044bbb641d368ebcfcd",
+    "url": "/clip1.png"
+  },
+  {
+    "revision": "d014156196945d998d01",
+    "url": "/css/app.a4ad3ca0.css"
+  },
+  {
+    "revision": "0b51ed0f66c9b0a7bfe8",
+    "url": "/css/post.1b4a2708.css"
+  },
+  {
+    "revision": "dff0bb00448b08eaeabb",
+    "url": "/css/posts.c6328364.css"
+  },
+  {
+    "revision": "9b4636814cb2ee3eba49",
+    "url": "/css/write.49441eb4.css"
+  },
+  {
+    "revision": "34a1f42a86fe21b627cdfcbbb7bde6e0",
+    "url": "/img/logo.34a1f42a.png"
+  },
+  {
+    "revision": "e6956db22f6b55500046d41bb128484a",
+    "url": "/img/mail.e6956db2.svg"
+  },
+  {
+    "revision": "04a9455d7b70e40d43c0cd371b53f68b",
+    "url": "/img/navBack.04a9455d.svg"
+  },
+  {
+    "revision": "e85c166faa1f10b64cb2e2b4abaca0c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d014156196945d998d01",
+    "url": "/js/app.f24fa4ee.js"
+  },
+  {
+    "revision": "f56969c45a40e345186d",
+    "url": "/js/chunk-vendors.b67c7387.js"
+  },
+  {
+    "revision": "0b51ed0f66c9b0a7bfe8",
+    "url": "/js/post.4a8f7827.js"
+  },
+  {
+    "revision": "dff0bb00448b08eaeabb",
+    "url": "/js/posts.5cbd84d5.js"
+  },
+  {
+    "revision": "9b4636814cb2ee3eba49",
+    "url": "/js/write.f1893491.js"
+  },
+  {
+    "revision": "a1d9062352e72c5fa61d6dcb461a3cd1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "8232508cadb679ca30b8b87786051c16",
+    "url": "/welcome.png"
+  }
+]);
